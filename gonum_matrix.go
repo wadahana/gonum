a87d066d5634d7f2bd414811dedd15a028626c4e
wadahana <wadahana@gmail.com>
2016-06-18 21:55:34 +0800

    change martix's data from major row to major col

--- gonum_matrix.go
+++ gonum_matrix.go
@@ -17,8 +17,9 @@ func NewMatrix(rows, cols int, data []float64) *Matrix {
     if size != len(data) {
         panic("NewMatrix, row * col not equal to length of data.")
     }
-    m.data = make([]float64, size, size);
-    copy(m.data, data);
+    // m.data = make([]float64, size, size);
+    // copy(m.data, data);
+    m.data = data;
     return &m;
 }
 
@@ -40,12 +41,12 @@ func (m *Matrix) GetElementNum() int {
 }
 
 func (m *Matrix) Get(row, col int) float64 {
-	i := row * m.col_num + col;
+	i := col * m.row_num + row;
 	return m.data[i]
 }
 
 func (m *Matrix) Set(row, col int, v float64) {
-    i := row + m.row_num*col;
+    i := col * m.row_num + row;
     m.data[i] = v;
 }
 
@@ -53,12 +54,12 @@ func (m *Matrix) getPtr() *[]float64 {
     return &(m.data);
 }
 
-func (m *Matrix) GetRow(row int) []float64 {
-    if row >= m.GetRowNum() {
-        panic("GetRow, row index large than row_num");
+func (m *Matrix) GetColumeData(col int) []float64 {
+    if col >= m.GetColumeNum() {
+        panic("GetColumeData, col index large than matrix's col_num");
     }
-    i := row * m.row_num;
-    return m.data[i : i + m.col_num];
+    i := col * m.row_num;
+    return m.data[i : i + m.row_num];
 }
 
 func (m *Matrix) GetData() []float64 {
@@ -72,11 +73,9 @@ func (m *Matrix) GetColumes(cols []int) *Matrix {
         }
     }
     data := make([]float64, 0);
-    for r := 0; r < m.row_num; r++ {
-        for _, c := range cols {
-            index := r * m.col_num + c;
-            data = append(data, m.data[index]);
-        }
+    for _, c := range cols {
+        i := c * m.row_num;
+        data = append(data, m.data[i : i + m.row_num]...);
     }
     sub := NewMatrix(m.row_num, len(cols), data);
     return sub;
@@ -86,39 +85,35 @@ func (m *Matrix) SwapRow(i, j int) {
     if i >= m.row_num || j >= m.row_num {
         panic("SwapRow(), i or j large than row nums");
     }
-    row_i := i * m.col_num;
-    row_j := j * m.col_num;
     for c := 0; c < m.col_num; c++ {
-        m.data[row_i + c], m.data[row_j + c] = m.data[row_j + c], m.data[row_i + c];
+        m.data[i + c * m.row_num], m.data[j + c * m.row_num] = m.data[j + c * m.row_num], m.data[i + c * m.row_num];
     }
 }
 
 func (m *Matrix) RBind(other *Matrix) *Matrix {
     if m.GetColumeNum() != other.GetColumeNum() {
-        panic("RBind(), 1st matrix's colume num NOT equal to 2nd matrix");
+        panic("RBind(), two matrix's colume num is diff");
     }
-    data := append(m.data, other.data...);
-    matrix := NewMatrix(m.GetRowNum() + other.GetRowNum(), m.GetColumeNum(), data);
+    col_num := m.GetColumeNum();
+    size := m.GetElementNum() + other.GetElementNum();
+    data := make([]float64, 0, size);
+    for i := 0; i < col_num; i++ {
+        d1 := m.GetColumeData(i);
+        d2 := other.GetColumeData(i)
+        data = append(data, d1...);
+        data = append(data, d2...);
+    }
+
+    matrix := NewMatrix(m.GetRowNum() + other.GetRowNum(), col_num, data);
     return matrix;
 }
 
 func (m *Matrix) CBind(other *Matrix) *Matrix {
     if m.GetRowNum() != other.GetRowNum() {
-        panic("RBind(), 1st matrix's row num NOT equal to 2nd matrix");
+        panic("RBind(), two matrix's row num is diff");
     }
-    size := m.GetElementNum() + other.GetElementNum();
-    data := make([]float64, 0, size);
-    c1 := m.GetColumeNum();
-    c2 := other.GetColumeNum();
-    i1 := 0;
-    i2 := 0;
-    for r := 0; r < m.GetRowNum(); r += 1 {
-        data = append(data, m.data[i1 : i1 + c1]...);
-        data = append(data, other.data[i2 : i2 + c2]...);
-        i1 = i1 + c1;
-        i2 = i2 + c2;
-    }
-    matrix := NewMatrix(m.GetRowNum(), c1 + c2, data);
+    data := append(m.data, other.data...);
+    matrix := NewMatrix(m.GetRowNum(), m.GetColumeNum() + other.GetColumeNum(), data);
     return matrix;
 }
 
@@ -126,7 +121,7 @@ func (m *Matrix) String() string {
     var s string;
     for r := 0; r < m.row_num; r++ {
         for c := 0; c < m.col_num; c++ {
-            s = s + fmt.Sprintf("%0.4f, ", m.data[r * m.col_num + c]);
+            s = s + fmt.Sprintf("%0.4f, ", m.data[c * m.row_num + r]);
         }
         s += "\n";
     }
--- gonum_matrix_math.go
+++ gonum_matrix_math.go
@@ -6,7 +6,6 @@ import (
 )
 
 func (m *Matrix) Diff(lag int) *Matrix {
-
     row_num := m.GetRowNum();
     col_num := m.GetColumeNum();
 
@@ -14,85 +13,97 @@ func (m *Matrix) Diff(lag int) *Matrix {
         panic("matrix.diff, lag large than row_num.");
     }
     size := (row_num - lag) * m.GetColumeNum();
-    data := make([]float64, size, size);
-    var i1 int = 0;
-    var i2 int = 0;
-    var lag2 int = lag * col_num;
-    for row := 0; row < row_num - lag; row += 1 {
-        for col := 0; col < col_num; col += 1 {
-            v1 := m.data[i2];
-            v2 := m.data[i2 + lag2];
-            data[i1] = v2 - v1;
-            i1 += 1;
-            i2 += 1;
+    data := make([]float64, 0, size);
+
+    for c := 0; c < col_num; c++ {
+        col := m.GetColumeData(c);
+        for i := 0 ; i < row_num - lag; i++ {
+            v1, v2 := col[i], col[i+lag];
+            data = append(data, (v2 - v1));
         }
     }
     dm := NewMatrix(row_num - lag, col_num, data);
     return dm;
 }
 
+//     var i1 int = 0;
+//     var i2 int = 0;
+//     var lag2 int = lag * col_num;
+//     for row := 0; row < row_num - lag; row += 1 {
+//         for col := 0; col < col_num; col += 1 {
+//             v1 := m.data[i2];
+//             v2 := m.data[i2 + lag2];
+//             data[i1] = v2 - v1;
+//             i1 += 1;
+//             i2 += 1;
+//         }
+//     }
+//     dm := NewMatrix(row_num - lag, col_num, data);
+//     return dm;
+// }
+
 func (m *Matrix) Log() *Matrix {
     size := m.GetRowNum() * m.GetColumeNum();
-    d2 := make([]float64, size, size);
-    d1 := m.GetData();
+    new_data := make([]float64, size, size);
+    data := m.GetData();
     for i := 0; i < size; i++ {
-        d2[i] = math.Log(d1[i]);
+        new_data[i] = math.Log(data[i]);
     }
-    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), d2);
+    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), new_data);
     return lm;
 }
 
 func (m *Matrix) Log2() *Matrix {
     size := m.GetRowNum() * m.GetColumeNum();
-    d2 := make([]float64, size, size);
-    d1 := m.GetData();
+    new_data := make([]float64, size, size);
+    data := m.GetData();
     for i := 0; i < size; i++ {
-        d2[i] = math.Log2(d1[i]);
+        new_data[i] = math.Log2(data[i]);
     }
-    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), d2);
+    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), new_data);
     return lm;
 }
 
 func (m *Matrix) Log10() *Matrix {
     size := m.GetRowNum() * m.GetColumeNum();
-    d2 := make([]float64, size, size);
-    d1 := m.GetData();
+    new_data := make([]float64, size, size);
+    data := m.GetData();
     for i := 0; i < size; i++ {
-        d2[i] = math.Log10(d1[i]);
+        new_data[i] = math.Log10(data[i]);
     }
-    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), d2);
+    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), new_data);
     return lm;
 }
 
 func (m *Matrix) Log1p() *Matrix {
     size := m.GetRowNum() * m.GetColumeNum();
-    d2 := make([]float64, size, size);
-    d1 := m.GetData();
+    new_data := make([]float64, size, size);
+    data := m.GetData();
     for i := 0; i < size; i++ {
-        d2[i] = math.Log1p(d1[i]);
+        new_data[i] = math.Log1p(data[i]);
     }
-    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), d2);
+    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), new_data);
     return lm;
 }
 
 func (m *Matrix) Sqrt() *Matrix {
     size := m.GetRowNum() * m.GetColumeNum();
-    d2 := make([]float64, size, size);
-    d1 := m.GetData();
+    new_data := make([]float64, size, size);
+    data := m.GetData();
     for i := 0; i < size; i++ {
-        d2[i] = math.Sqrt(d1[i]);
+        new_data[i] = math.Sqrt(data[i]);
     }
-    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), d2);
+    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), new_data);
     return lm;
 }
 
 func (m *Matrix) Pow(exp float64) *Matrix {
     size := m.GetRowNum() * m.GetColumeNum();
-    d2 := make([]float64, size, size);
-    d1 := m.GetData();
+    new_data := make([]float64, size, size);
+    data := m.GetData();
     for i := 0; i < size; i++ {
-        d2[i] = math.Pow(d1[i], exp);
+        new_data[i] = math.Pow(data[i], exp);
     }
-    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), d2);
+    lm := NewMatrix(m.GetRowNum(), m.GetColumeNum(), new_data);
     return lm;
 }
